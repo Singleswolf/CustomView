e77ffee52d08ff55a0f2e94afd934494aa47abf0
yong <1277867253@qq.com>
2019-03-19 12:28:32 +0800

    update ItemView

--- library/src/main/java/com/zyong/library/widget/ItemView.java
+++ library/src/main/java/com/zyong/library/widget/ItemView.java
@@ -1,13 +1,13 @@
 package com.zyong.library.widget;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
@@ -27,14 +27,13 @@ public class ItemView extends RelativeLayout {
     private int mDefaultSize;
     private ImageView mIvLeftIcon;
     private TextView mTvLeft;
-    private ImageView mIvRightArrow;
+    private ImageView mIvRightIcon;
     private TextView mTvRight;
     private Switch mSwitch;
     private View mDivide;
     private int styleType;
-    private OnItemViewClickListener mClickListener;
+    private OnClickListener mClickListener;
     private View mItemView;
-    private OnCheckedChangedListener mChangedListener;
 
     public ItemView(Context context) {
         super(context);
@@ -62,7 +61,7 @@ public class ItemView extends RelativeLayout {
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ItemView);
             Drawable leftIcon = typedArray.getDrawable(R.styleable.ItemView_left_icon);
             if (leftIcon != null) {
-                mIvLeftIcon.setImageDrawable(leftIcon);
+                mIvLeftIcon.setBackground(leftIcon);
             }
             boolean leftIconVisible = typedArray.getBoolean(R.styleable.ItemView_left_icon_visible, true);
             mIvLeftIcon.setVisibility(leftIconVisible ? VISIBLE : GONE);
@@ -74,8 +73,8 @@ public class ItemView extends RelativeLayout {
             if (leftTxtSize != -1) {
                 mTvLeft.setTextSize(TypedValue.COMPLEX_UNIT_PX, leftTxtSize);
             }
-            int leftTxtColor = typedArray.getColor(R.styleable.ItemView_left_txt_color, 0);
-            if (leftTxtColor != 0) {
+            ColorStateList leftTxtColor = typedArray.getColorStateList(R.styleable.ItemView_left_txt_color);
+            if (leftTxtColor != null) {
                 mTvLeft.setTextColor(leftTxtColor);
             }
             String rightTxt = typedArray.getString(R.styleable.ItemView_right_txt);
@@ -86,18 +85,22 @@ public class ItemView extends RelativeLayout {
             if (rightTxtSize != -1) {
                 mTvRight.setTextSize(TypedValue.COMPLEX_UNIT_PX, rightTxtSize);
             }
-            int rightTxtColor = typedArray.getColor(R.styleable.ItemView_right_txt_color, 0);
-            if (rightTxtColor != 0) {
+            ColorStateList rightTxtColor = typedArray.getColorStateList(R.styleable.ItemView_right_txt_color);
+            if (rightTxtColor != null) {
                 mTvRight.setTextColor(rightTxtColor);
             }
-            Drawable rightArrow = typedArray.getDrawable(R.styleable.ItemView_right_arrow);
-            if (null != rightArrow) {
-                mIvRightArrow.setImageDrawable(rightArrow);
+            Drawable rightIcon = typedArray.getDrawable(R.styleable.ItemView_right_icon);
+            if (null != rightIcon) {
+                mIvRightIcon.setBackground(rightIcon);
             }
-            boolean rightArrowVisible = typedArray.getBoolean(R.styleable.ItemView_right_arrow_visible, true);
-            mIvRightArrow.setVisibility(rightArrowVisible ? VISIBLE : GONE);
+            boolean rightIconVisible = typedArray.getBoolean(R.styleable.ItemView_right_icon_visible, true);
+            mIvRightIcon.setVisibility(rightIconVisible ? VISIBLE : GONE);
             boolean divideVisible = typedArray.getBoolean(R.styleable.ItemView_divide_line, true);
             mDivide.setVisibility(divideVisible ? VISIBLE : GONE);
+            boolean aBoolean = typedArray.getBoolean(R.styleable.ItemView_custom_switch, false);
+            if (aBoolean) {
+                setCustomSwitch();
+            }
             styleType = typedArray.getInt(R.styleable.ItemView_right_style, 0);
             setItemType(styleType);
             typedArray.recycle();
@@ -109,12 +112,17 @@ public class ItemView extends RelativeLayout {
         mItemView = inflate(context, R.layout.item_view, this);
         mIvLeftIcon = (ImageView) findViewById(R.id.iv_left_icon);
         mTvLeft = (TextView) findViewById(R.id.tv_left);
-        mIvRightArrow = (ImageView) findViewById(R.id.iv_right_arrow);
+        mIvRightIcon = (ImageView) findViewById(R.id.iv_right_icon);
         mTvRight = (TextView) findViewById(R.id.tv_right);
         mSwitch = (Switch) findViewById(R.id.switch_view);
         mDivide = (View) findViewById(R.id.divide);
     }
 
+    public void setCustomSwitch() {
+        mSwitch.setThumbDrawable(getContext().getResources().getDrawable(R.drawable.item_switch_click));
+        mSwitch.setTrackResource(android.R.color.transparent);
+        mSwitch.setBackground(getContext().getResources().getDrawable(R.drawable.item_switch_bg));
+    }
 
     public void setLeftIcon(Drawable leftIcon) {
         mIvLeftIcon.setBackground(leftIcon);
@@ -148,13 +156,13 @@ public class ItemView extends RelativeLayout {
         mTvRight.setTextColor(txtColor);
     }
 
-    public void setRightArrow(Drawable rightArrow) {
-        mIvRightArrow.setBackground(rightArrow);
+    public void setRightIcon(Drawable rightIcon) {
+        mIvRightIcon.setBackground(rightIcon);
     }
 
-    public void setRightArrowVisible(boolean visible) {
+    public void setRightIconVisible(boolean visible) {
         if (styleType == NORMAL) {
-            mIvRightArrow.setVisibility(visible ? VISIBLE : GONE);
+            mIvRightIcon.setVisibility(visible ? VISIBLE : GONE);
         }
     }
 
@@ -170,12 +178,12 @@ public class ItemView extends RelativeLayout {
         this.styleType = styleType;
         if (styleType == NORMAL) {//normal
             mTvRight.setVisibility(VISIBLE);
-            mIvRightArrow.setVisibility(VISIBLE);
+            mIvRightIcon.setVisibility(VISIBLE);
             mSwitch.setVisibility(GONE);
             mItemView.setClickable(true);
         } else if (styleType == SWITCH) {//switch
             mTvRight.setVisibility(GONE);
-            mIvRightArrow.setVisibility(GONE);
+            mIvRightIcon.setVisibility(GONE);
             mSwitch.setVisibility(VISIBLE);
             mItemView.setClickable(false);
         }
@@ -185,35 +193,36 @@ public class ItemView extends RelativeLayout {
         mDivide.setVisibility(visible ? VISIBLE : GONE);
     }
 
-    public interface OnItemViewClickListener {
-        void onClick(View v);
+    public void setEnable(boolean enable) {
+        mSwitch.setEnabled(enable);
+        mTvLeft.setEnabled(enable);
+        mItemView.setEnabled(enable);
     }
 
-    public void setOnItemViewClickListener(OnItemViewClickListener listener) {
-        mItemView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mClickListener != null) {
-                    mClickListener.onClick(v);
+    public interface OnClickListener {
+        void onClick(View view);
+    }
+
+    public void setListener(OnClickListener changedListener) {
+        if (styleType == NORMAL) {
+            mItemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mClickListener != null) {
+                        mClickListener.onClick(mItemView);
+                    }
                 }
-            }
-        });
-        this.mClickListener = listener;
-    }
-
-    public interface OnCheckedChangedListener {
-        void onCheckedChanged(boolean checked);
-    }
-
-    public void setOnCheckedChangedListener(OnCheckedChangedListener changedListener) {
-        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (mChangedListener != null) {
-                    mChangedListener.onCheckedChanged(isChecked);
+            });
+        } else {
+            mSwitch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mClickListener != null) {
+                        mClickListener.onClick(mItemView);
+                    }
                 }
-            }
-        });
-        this.mChangedListener = changedListener;
+            });
+        }
+        this.mClickListener = changedListener;
     }
 }
