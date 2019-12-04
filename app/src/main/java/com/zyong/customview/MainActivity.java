2b8c51be2768721b932edfba6aec88748b693da4
yong <1277867253@qq.com>
2019-12-04 12:17:32 +0800

    support androidx

--- app/src/main/java/com/zyong/customview/MainActivity.java
+++ app/src/main/java/com/zyong/customview/MainActivity.java
@@ -1,13 +1,14 @@
 package com.zyong.customview;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.zyong.library.widget.AutoPollRecyclerView;
-import com.zyong.library.widget.BaseRecyclerView;
+import com.zyong.library.base.BaseRecyclerView;
 import com.zyong.library.widget.ItemView;
 import com.zyong.library.widget.TitleView;
 
--- library/src/main/java/com/zyong/library/base/BaseRecyclerView.java
+++ library/src/main/java/com/zyong/library/base/BaseRecyclerView.java
@@ -1,12 +1,13 @@
-package com.zyong.library.widget;
+package com.zyong.library.base;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * @Description:
